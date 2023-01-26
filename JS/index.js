2928174b28d25690cf856565a50b376dcc1e8221
Shawn McNulty <smcnulty75@gmail.com>
2023-01-26 07:52:30 -0500

    Fixed

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,12 @@
-console.log("your index.js file is loaded correctly");
-$(document).ready(function(){
-    $( ".navitem" ).click(function() {
-         console.log("this has been clicked");
+console.log('your index.js file is loaded correctly');
+$(document).ready(function () {
+  $('.navitem').click(function () {
+    console.log('this has been clicked');
+    debugger;
 
- ;
-        $(this).toggleClass("active");
-        if ($(this).hasClass("active")) {
-          $(this).animate({letterSpacing: "+=10px"});
-          $(this).animate({letterSpacing: "-=10px"});
-        }
-        else {
-        
-          $(this).removeClass("active")
-          
-          $(this).animate({letterSpacing: "+=10px"})
-          $(this).animate({letterSpacing: "-=10px"});
-        }
-      
-      });
-   
+    $('.active').animate({ letterSpacing: '-=10px' });
+    $(this).siblings('.active').removeClass('active');
+    $(this).toggleClass('active');
+    $('.active').animate({ letterSpacing: '+=10px' });
   });
+});
